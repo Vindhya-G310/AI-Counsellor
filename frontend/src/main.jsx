a3b86ebace36ae3361df45fc98af2889757a04ab
vindya gopanna <vindhyagopanna3102@gmail.com>
2026-01-31 01:30:21 +0530

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-i;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
